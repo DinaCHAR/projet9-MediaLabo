04a0a008cb343c6f82bcf5d2ec9adc83b51facc8
Dina CHARKI <dina.charki@eskerpartners.com>
2026-01-18 16:26:18 +0100

    creation microservice patient

---
+++ micorservice-patient/src/main/java/org/medilabo/patient/micorservice_patient/MicorservicePatientApplication.java
@@ -0,0 +1,13 @@
+package org.medilabo.patient.micorservice_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicorservicePatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicorservicePatientApplication.class, args);
+	}
+
+}
---
+++ micorservice-patient/src/test/java/org/medilabo/patient/micorservice_patient/MicorservicePatientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.medilabo.patient.micorservice_patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicorservicePatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
